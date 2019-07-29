5465d103226a29a7b81cd6ab2644fc26354973bb
BshanYey <596995384@qq.com>
2019-07-29 17:39:56 +0800

    UTF-8 bom -> UTF-8

---
+++ bshanyey/src/main/java/com/accumulation/bshanyey/utils/UTF8Utils.java
@@ -0,0 +1,28 @@
+package com.accumulation.bshanyey.utils;
+
+/**
+ * UTF-8工具类
+ */
+public class UTF8Utils {
+
+
+    /**
+     * UTF-8 BOM 转 UTF-8
+     *      保留原文件，新文件为UTF-8无BOM编码
+     * @param fromPath
+     * @param toPath
+     */
+    public static void conversion(String fromPath, String toPath){
+
+    }
+
+    /**
+     * UTF-8 BOM 转 UTF-8
+     *      替换原文件
+     * @param fromPath
+     */
+    public static void conversion(String fromPath){
+
+    }
+
+}
