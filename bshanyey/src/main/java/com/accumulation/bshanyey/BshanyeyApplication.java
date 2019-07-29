be3073bc5b2356ffb5d6bbe677dcba348d956f19
BshanYey <596995384@qq.com>
2019-07-29 17:40:46 +0800

    UTF-8 bom -> UTF-8

---
+++ bshanyey/src/main/java/com/accumulation/bshanyey/BshanyeyApplication.java
@@ -0,0 +1,13 @@
+package com.accumulation.bshanyey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BshanyeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BshanyeyApplication.class, args);
+    }
+
+}
