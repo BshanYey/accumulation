66fd4876e9f5c3c127b8fa7a697448b14536ccf5
BshanYey <596995384@qq.com>
2019-07-30 18:18:41 +0800

    UTF-8 bom -> UTF-8

--- bshanyey/src/main/java/com/accumulation/bshanyey/utils/UTF8Utils.java
+++ bshanyey/src/main/java/com/accumulation/bshanyey/utils/UTF8Utils.java
@@ -1,5 +1,9 @@
 package com.accumulation.bshanyey.utils;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 /**
  * UTF-8工具类
  */
@@ -13,6 +17,68 @@ public class UTF8Utils {
      * @param toPath
      */
     public static void conversion(String fromPath, String toPath){
+        File fromFile = new File(fromPath);
+        File toFile = new File(toPath);
+        if(!fromFile.exists()){
+            return;
+        }
+        if(fromFile.isFile()){
+            if(!toFile.exists()){
+                toFile.getParentFile().mkdirs();
+            }
+            conversionFile2File(fromFile, toFile);
+            return;
+        }
+        if(fromFile.isDirectory()){
+            if(!toFile.exists()){
+                toFile.mkdirs();
+            }
+            for(File file : fromFile.listFiles()){
+                conversion(file.getPath(), toPath + "/" + file.getName());
+            }
+        }
+    }
+
+    /**
+     * 将utf-8 bom编码的文件 复制为一个utf-8无bom编码的文件 内容不变
+     * @param fromFile
+     * @param toFile
+     */
+    private static void conversionFile2File(File fromFile, File toFile){
+
+        try {
+            InputStream in = new BufferedInputStream(new FileInputStream(fromFile));
+            OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(toFile));
+
+            byte[] content0 = new byte[1024];
+            boolean flag = true;
+            while (true){
+                int len = in.read(content0);
+                if(len == -1){
+                    break;
+                }
+                String con;
+                // 去bom
+                if(flag && content0[0] == -17 && content0[1] == -69 && content0[2] == -65){
+                    flag = false;
+                    con = new String(content0, 3, len - 3);
+                } else {
+                    con = new String(content0, 0, len);
+                }
+
+                // 写出
+                out.write(new String(con));
+                out.flush();
+            }
+
+
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -22,6 +88,60 @@ public class UTF8Utils {
      * @param fromPath
      */
     public static void conversion(String fromPath){
+        File fromFile = new File(fromPath);
+        if(!fromFile.exists()){
+            return;
+        }
+        if(fromFile.isFile()){
+            conversionFile2File(fromFile);
+            return;
+        }
+        if(fromFile.isDirectory()){
+            for(File file : fromFile.listFiles()){
+                conversion(file.getPath());
+            }
+        }
+    }
+
+    /**
+     * 将utf-8 bom编码的文件 复制为一个utf-8无bom编码的文件 内容不变
+     * @param fromFile
+     */
+    private static void conversionFile2File(File fromFile){
+//
+//        try {
+//            InputStream in = new BufferedInputStream(new FileInputStream(fromFile));
+//            OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(toFile));
+//
+//            byte[] content0 = new byte[1024];
+//            boolean flag = true;
+//            while (true){
+//                int len = in.read(content0);
+//                if(len == -1){
+//                    break;
+//                }
+//                String con;
+//                // 去bom
+//                if(flag && content0[0] == -17 && content0[1] == -69 && content0[2] == -65){
+//                    flag = false;
+//                    con = new String(content0, 3, len - 3);
+//                } else {
+//                    con = new String(content0, 0, len);
+//                }
+//
+//                // 写出
+//                out.write(new String(con));
+//                out.flush();
+//            }
+//
+//
+//            in.close();
+//            out.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
--- bshanyey/src/test/java/com/accumulation/bshanyey/BshanyeyApplicationTests.java
+++ bshanyey/src/test/java/com/accumulation/bshanyey/BshanyeyApplicationTests.java
@@ -1,5 +1,6 @@
 package com.accumulation.bshanyey;
 
+import com.accumulation.bshanyey.utils.UTF8Utils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,55 +14,7 @@ public class BshanyeyApplicationTests {
 
     @Test
     public void contextLoads() {
+        UTF8Utils.conversion("C:\\Users\\coder\\Desktop\\temp\\test1", "C:\\Users\\coder\\Desktop\\temp\\test2");
     }
 
-    @Test
-    public void testFile(){
-
-        // 将utf-8 bom编码的文件 复制为一个utf-8无bom编码的文件 内容不变
-        try {
-            InputStream in0 = new BufferedInputStream(new FileInputStream("C:\\Users\\coder\\Desktop\\temp\\t1.java"));
-            InputStream in1 = new BufferedInputStream(new FileInputStream("C:\\Users\\coder\\Desktop\\temp\\t2.java"));
-            OutputStream out = new BufferedOutputStream(new FileOutputStream("C:\\Users\\coder\\Desktop\\temp\\t3.java"));
-            OutputStreamWriter o1 = new OutputStreamWriter(new FileOutputStream("C:\\Users\\coder\\Desktop\\temp\\t3.java"));
-
-            byte[] content0 = new byte[1024];
-            boolean flag = true;
-            while (true){
-                int len = in0.read(content0);
-                if(len == -1){
-                    break;
-                }
-                String con;
-                // 去bom
-                if(flag && content0[0] == -17 && content0[1] == -69 && content0[2] == -65){
-                    flag = false;
-                    con = new String(content0, 3, len - 3);
-                } else {
-                    con = new String(content0, 0, len);
-                }
-
-                // 写出
-//                out.write(con);
-                o1.write(new String(con));
-                o1.flush();
-                System.out.println();
-            }
-
-
-//            OutputStreamWriter ow = new OutputStreamWriter(out, "UTF-8");
-//            ow.write(new String());
-//            ow.flush();
-            System.out.println();
-            in0.close();
-            in1.close();
-//            ow.close();
-            out.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
